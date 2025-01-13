a8d3285cd7e7ab8bfd9a8e5e6779800b175bcdc3
mattchudani <matthewtablad972@gmail.com>
2025-01-13 13:12:37 -1000

    Clock Finished

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ Created: 1/7/25
 Bugs Known: none
 */
 
-/* --- FUNCTIONS --- */
+/* --- VARIABLES --- */
 
-$(document).ready(function() {
+
+/* --- FUNCTIONS --- */
+/* Ready */
+
+window.onload = function() {
     function displayTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridiem = "AM";
+        var clockDiv = document.getElementById('clock');
+
+       
+        
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+        }
+
+        if (hours === 0) {
+            hours = 12;    
+        }
+
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
     }
-});
+    displayTime();
+    setInterval(displayTime, 1000);
+};
